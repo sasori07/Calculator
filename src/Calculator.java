4966fa7bca6ea1642da0a8b3415eaa070fb6154e
sasori07 <sasori07@hotmail.com>
2019-01-19 12:42:38 -0500

    Add substract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,12 @@
 public class Calculator {
 
 
-    public static double add(double num1 , double num2){
-        return num1+num2 ;
+    public static double add(double num1,double num2) {
+        return num1+num2;
 
     }
 
-
+    public static double subtract(double num1,double num2) {
+        return num1 -num2;
+    }
 }
